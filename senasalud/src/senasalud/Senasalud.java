90f0287bc3aaa2163ead1b69cbb27f794df0d74f
sebastianAcosta <sala@HOR-AULA501-007.unihorizonte.salas>
2019-10-29 16:53:05 -0500

    my second commit yeahhh

--- senasalud/src/senasalud/Senasalud.java
+++ senasalud/src/senasalud/Senasalud.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package senasalud;
 
-/**
- *
- * @author sala
- */
-public class Senasalud {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Senasalud {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        String nombre;
+        Scanner n = new Scanner(System.in);
+        System.out.println("digite su nombre");
+        nombre= n.next();
+        System.out.println("su nombre es:"+nombre);
+        
+        
+        String as;
+        Scanner no = new Scanner(System.in);
+        System.out.println("ingrese su apellido");
+        as= no.next();
+        System.out.println("su apellido es:"+as);
+        
+        
+        int aso;
+        Scanner nin = new Scanner(System.in);
+        System.out.println("ingrese un numero");
+        aso = nin.nextByte();
+        
+        for (int i = 0; i <= 10; i++) {
+            int m=aso*i;
+            System.out.println(aso+" x "+i+" = "+m);
+        }
+
+        
+             
+        
     }
     
 }
